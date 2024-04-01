b56f6d9bef3209966839b3f47c2294f0e175a4e5
Supriyaratnaparkhe <91657954+Supriyaratnaparkhe@users.noreply.github.com>
2024-04-01 19:44:38 +0530

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,11 +4,9 @@ dotenv.config();
 
 const authenticate = (req, res, next) => {
     const { token } = req.headers;
-    // console.log('Received token:', token);
     if (!token) {
         return res.status(401).json({ error: "unauthorized Or missing token" });
     }
-
     try {
         req.user = jwt.verify(token, process.env.JWT_Token).user;
         next();
